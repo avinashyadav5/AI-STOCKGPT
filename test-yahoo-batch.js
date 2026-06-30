66ae0298ec42830a51cae7fff818ee5a9371f7e3
Avinash Yadav <aviydv7518@gmail.com>
2026-06-30 13:00:46 +0530

    chore: clean up root directory scripts

--- test-yahoo-batch.js
+++
@@ -1,4 +0,0 @@
-fetch('https://query1.finance.yahoo.com/v7/finance/quote?symbols=TATAMOTORS.NS,AAPL')
-  .then(res => res.json())
-  .then(json => console.log("Result:", JSON.stringify(json, null, 2)))
-  .catch(err => console.error(err));
